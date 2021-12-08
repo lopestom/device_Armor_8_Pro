b2658afa67c7ff6e5e3485c20a2fa3f74bbd7600
lopestorm <lopestom1@gmail.com>
2021-12-08 02:45:35 -0300

    Update twrp_Armor_8_Pro.mk

--- twrp_Armor_8_Pro.mk
+++ twrp_Armor_8_Pro.mk
@@ -24,8 +24,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
-	$(LOCAL_PATH)/prebuilt/dtb:dtb.img
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
+#\
+#	$(LOCAL_PATH)/prebuilt/dtb:dtb.img
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Armor_8_Pro
